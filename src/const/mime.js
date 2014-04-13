6547df494490e0a5d7341233fb23779ddab30050
Alex Kit <source.kit@gmail.com>
2014-04-14 00:58:21 +0200

    * content-type + charset header

--- src/const/mime.js
+++ src/const/mime.js
@@ -1,4 +1,14 @@
-var mime_JSON = 'application/json',
-	mime_HTML = 'text/html',
-	mime_PLAIN = 'text/plain'
+
+var mime_JSON,
+	mime_HTML,
+	mime_PLAIN
 	;
+
+(function(){
+	var utf8 = ';charset=utf-8';
+
+	mime_JSON = 'application/json' + utf8;
+	mime_HTML = 'text/html' + utf8;
+	mime_PLAIN = 'text/plain' + utf8;
+	
+}());
