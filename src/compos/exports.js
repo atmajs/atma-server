95bd0babbe9068dbd4efa6ddb0371ceff0f15995
Alex Kit <source.kit@gmail.com>
2016-03-07 17:01:54 +0100

    v0.1.36

--- src/compos/exports.js
+++
@@ -1,9 +0,0 @@
-
-function initilizeEmbeddedComponents(app) {
-	initilizeEmbeddedComponents = function(){};
-	
-	var DEBUG = app.config.$is('debug'),
-		BUILD_DIR = '/public/build/'
-		;
-	// import resources/exports.js
-}
