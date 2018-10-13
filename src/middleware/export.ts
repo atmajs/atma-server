7fa6a16273130f51ec6ca493abc48f09b0e6afff
Alex Kit <source.kit@gmail.com>
2018-10-13 16:01:24 +0200

    v0.4.3

--- src/middleware/export.ts
+++
@@ -1,7 +0,0 @@
-import queryMidd from './query'
-import staticMidd from './static'
-
-export default {
-    query: queryMidd,
-    static: staticMidd
-};
--- tools/build-dts.js
+++
@@ -1,9 +0,0 @@
-var dts = require('dts-bundle');
-
-dts.bundle({
-	name: 'atma-server',
-	main: './ts-temp/export.d.ts',
-	out: './typings/index.d.ts'
-});
-
-io.File.copyTo('./ts-temp/typings/index.d.ts', './lib/server.d.ts');
