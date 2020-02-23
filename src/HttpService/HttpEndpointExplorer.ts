628ba93d0bca05b32ce10f54e93dd7bb4fc1f0a1
Alex Kit <source.kit@gmail.com>
2020-02-23 01:41:34 +0100

    publish: fix (endpoint explorer) register correct paths

--- src/HttpService/HttpEndpointExplorer.ts
+++ src/HttpService/HttpEndpointExplorer.ts
@@ -1,4 +1,4 @@
-import { env } from 'atma-io'
+import { File, env } from 'atma-io'
 import { class_Uri } from 'atma-utils'
 import { fs_exploreFiles } from '../util/fs'
 import alot from 'alot'
@@ -20,15 +20,16 @@ export namespace HttpEndpointExplorer {
         }
 
         let files = await fs_exploreFiles(path);
-        let keyValues = await alot(files).mapAsync(async file => {
-            let str = await file.readAsync <string> ({ skipHooks: true });
+        let keyValues = await alot(files).mapAsync(async $file => {
+            let file = new File($file.uri.toString(), { cached: false });
+            let str = await file.readAsync <string> ({ skipHooks: true});
             let rgx = /@[\w\.]+.route\s*\(\s*['"]([^'"]+)['"]/;
             let match = rgx.exec(str);
             if (match == null) {
                 return null;
             }
             let urlPattern = match[1];
-            return [ urlPattern, file.uri.toString() ];
+            return [ `^${urlPattern}`, file.uri.toString() ];
         }).toArrayAsync({ threads: 4 })
 
         return alot(keyValues).toDictionary(arr => arr[0], arr => arr[1]);
